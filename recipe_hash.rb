320953cc05f11abd3e6e3bdd52e77b19a6c2e81b
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-24 21:09:18 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -17,3 +17,24 @@ end
 recipe_ingredients
 
 
+def amount_of_chocolate_chips
+  return chocolate_chips = $perfect_10_recipe.values[7]
+end 
+
+def ingredients_and_amounts
+  $perfect_10_recipe.each do |:ingredient, "amount"|
+  puts "You will need #{amount} of #{ingredient}."
+end
+
+def ingredients
+  return ingredient = $perfect_10_recipe.each do |:ingredient|
+end 
+
+def amounts(perfect_10_recipe)
+  return amounts = $perfect_10_recipe.each do |"amount"|
+end
+
+def number_ingredients(perfect_10_recipe)
+  return
+end 
+
