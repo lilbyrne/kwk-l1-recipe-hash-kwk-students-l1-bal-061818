0057d6913ed154cf8c94797bffe6ebbcb2141f98
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-24 21:06:55 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,4 +1,4 @@
-$perfect_10_recipe = {
+perfect_10_recipe = {
   :almond_flour => "2 cups",
   :gluten_free_whole_oats => "1 cup",
   :kosher_salt => "1/2 tea spoon",
@@ -11,8 +11,8 @@ $perfect_10_recipe = {
   :agave => "1/4 cup",
 }
 
-def recipe_ingredients($perfect_10_recipe)
-  return $perfect_10_recipe
+def recipe_ingredients(perfect_10_recipe)
+  return perfect_10_recipe
 end 
 recipe_ingredients
 
